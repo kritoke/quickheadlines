26699bf51ef4df96a90cdbb11107ca7c5a7ef930
kritoke <kritoke@users.noreply.github.com>
2026-02-08 11:20:25 -0600

    feat(ui): use task-based fetch with retry and add error state to Home

--- ui/src/Pages/Home/index.elm
+++ ui/src/Pages/Home/index.elm
@@ -1,39 +1,47 @@
 
 module Pages.Home exposing (Model, Msg, main)
 
-import Api.News exposing (Cluster, fetchClustersCmd)
+import Api.News exposing (Cluster)
+import Backend.Clusters as BC
 import Browser
 import Html exposing (Html, div, text, ul, li)
 import Http
+import Task
+import Maybe exposing (Maybe(..))
 
 
-type alias Model = { clusters : List Cluster, loading : Bool }
+type alias Model = { clusters : List Cluster, loading : Bool, error : Maybe String }
 
 type Msg
     = GotClusters (Result Http.Error (List Cluster))
 
 init : ( Model, Cmd Msg )
 init =
-    ( { clusters = [], loading = True }
-    , fetchClustersCmd "/api/clusters" GotClusters
+    ( { clusters = [], loading = True, error = Nothing }
+    , Task.attempt GotClusters (BC.fetchClustersTaskWithRetry 2 "/api/clusters")
     )
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotClusters (Ok ls) ->
-            ( { model | clusters = ls, loading = False }, Cmd.none )
+            ( { model | clusters = ls, loading = False, error = Nothing }, Cmd.none )
 
         GotClusters (Err _) ->
-            ( { model | loading = False }, Cmd.none )
+            ( { model | loading = False, error = Just "Failed to fetch clusters" }, Cmd.none )
 
 view : Model -> Html Msg
 view model =
     if model.loading then
         div [] [ text "Loading clusters..." ]
     else
-        div []
-            [ ul [] (List.map (\c -> li [] [ text c.headline ]) model.clusters) ]
+        case model.error of
+            Just err ->
+                div [] [ text err ]
+
+            Nothing ->
+                div []
+                    [ ul [] (List.map (\c -> li [] [ text c.headline ]) model.clusters) ]
 
 main : Program () Model Msg
 main =
