b2930ad9bb55426700aee1843f03dddb507c8c56
kritoke <kritoke@users.noreply.github.com>
2026-02-17 07:58:16 -0600

    Update default cache settings: retention to 336h (14 days), document all config options

--- src/web/assets.cr
+++ src/web/assets.cr
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # Build: 2026-02-17T07:56:50-06:00
 =======
 # Build: 2026-02-17T06:44:25-06:00
 >>>>>>> 22b6938 (Remove deprecated root package.json and package-lock.json)
+=======
+# Build: 2026-02-17T07:56:50-06:00
+>>>>>>> aebe4cc (Update default cache settings: retention to 336h (14 days), document all config options)
 # This comment is updated before each build to force BakedFileSystem recompilation
 # DO NOT remove this line - it's used by the build system
 
