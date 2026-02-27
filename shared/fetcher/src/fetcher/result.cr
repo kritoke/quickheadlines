69f3d512279f58ef826f2667086cad9c66937549
kritoke <kritoke@users.noreply.github.com>
2026-02-27 05:47:51 -0600

    Extract fetcher to kritoke/fetcher.cr shard, remove shared folder
    - Update shard.yml to depend on github.com/kritoke/fetcher.cr
    - Remove shared/fetcher directory (now in separate repo)
    - FP refactor: Result types, entity records, clustering engine

--- shared/fetcher/src/fetcher/result.cr
+++
@@ -1,9 +0,0 @@
-module Fetcher
-  record Result,
-    entries : Array(Entry),
-    etag : String?,
-    last_modified : String?,
-    site_link : String?,
-    favicon : String?,
-    error_message : String?
-end
