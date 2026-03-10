d4a4238fcbe23d01b82de4f75d3e2fea21959a49
kritoke <kritoke@users.noreply.github.com>
2026-03-10 05:39:33 -0500

    chore: rebuild assets

--- src/web/assets.cr
+++ src/web/assets.cr
@@ -1,4 +1,4 @@
-# Build: 2026-03-10T05:26:09-05:00
+# Build: 2026-03-10T05:33:08-05:00
 # This comment is updated before each build to force BakedFileSystem recompilation
 # DO NOT remove this line - it's used by the build system
 
