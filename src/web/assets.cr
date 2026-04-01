d6a75010490b407f1d16c44e02187fdb93eccf1f
kritoke <kritoke@users.noreply.github.com>
2026-04-01 06:06:26 -0500

    Update vug dependency from 0.2.0 to 0.3.0

--- src/web/assets.cr
+++ src/web/assets.cr
@@ -1,4 +1,4 @@
-# Build: 2026-04-01T06:01:11-05:00
+# Build: 2026-04-01T06:04:57-05:00
 # This comment is updated before each build to force BakedFileSystem recompilation
 # DO NOT remove this line - it's used by the build system
 
