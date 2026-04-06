80903567a62a08547204016522e03d8c70ccc323
kritoke <kritoke@users.noreply.github.com>
2026-04-05 19:12:13 -0500

    Fix software releases to only appear in configured tabs
    - Remove fallback that copies tab software releases to global state
    - Only return software_releases for specific tab that has them configured
    - Software releases now only appear in Dev tab, not all tabs

--- src/controllers/feeds_controller.cr
+++ src/controllers/feeds_controller.cr
@@ -49,9 +49,18 @@ class QuickHeadlines::Controllers::FeedsController < QuickHeadlines::Controllers
                        end
                      end
 
-    software_releases_response = software_releases_snapshot.map do |feed|
-      Api.feed_to_response(feed, "", cache.item_count(feed.url), item_limit)
-    end
+    software_releases_response = if active_tab.to_s.downcase == "all"
+                                   [] of FeedResponse
+                                 else
+                                   tab_with_sr = tabs_snapshot.find { |tab| tab.name.downcase == active_tab.downcase }
+                                   if tab_with_sr && tab_with_sr.software_releases.any?
+                                     tab_with_sr.software_releases.map do |feed|
+                                       Api.feed_to_response(feed, active_tab, cache.item_count(feed.url), item_limit)
+                                     end
+                                   else
+                                     [] of FeedResponse
+                                   end
+                                 end
 
     FeedsPageResponse.new(
       tabs: tabs_response,
