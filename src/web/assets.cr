a3928f1b2edfa15bbb708f92fde48953f89e6c24
kritoke <kritoke@users.noreply.github.com>
2026-04-09 04:39:30 -0500

    fix console warnings

--- src/web/assets.cr
+++ src/web/assets.cr
@@ -1,4 +1,4 @@
-# Build: 2026-04-08T20:21:09-05:00
+# Build: 2026-04-08T20:24:14-05:00
 # This comment is updated before each build to force BakedFileSystem recompilation
 # DO NOT remove this line - it's used by the build system
 
