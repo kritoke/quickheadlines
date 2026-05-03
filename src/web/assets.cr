e0998a106ad971588745eba03c6cdb8d226136de
kritoke <kritoke@users.noreply.github.com>
2026-05-02 21:46:00 -0500

    Rebuild assets with corrected Azurite constant names

--- src/web/assets.cr
+++ src/web/assets.cr
@@ -1,4 +1,4 @@
-# Build: 2026-05-02T17:08:16-05:00
+# Build: 2026-05-02T21:44:26-05:00
 # This comment is updated before each build to force BakedFileSystem recompilation
 # DO NOT remove this line - it's used by the build system
 
